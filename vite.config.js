80dea7649346e7248bd482bdb206645142916ca7
Abdisa.dev <meetabdisa@gmail.com>
2025-09-05 13:28:59 +0300

    updated the vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/devabdisa/React-powered-Investment-Calculator', // 👈 important
+  base: '/React-powered-Investment-Calculator', // 👈 important
 });
 
